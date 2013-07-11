8841f263f66e0a23f39c019b730c365828672c76
bigdaub <taylorsminor@gmail.com>
2013-07-11 00:30:37 -0400

    First Commit
    Changed Names, Updated Dev Info, Block Explorer, Market API Price, and
    Images

--- integration-android/src/de/schildbach/wallet/bbqcoin/integration/android/BbqcoinIntegration.java
+++ integration-android/src/de/schildbach/wallet/bbqcoin/integration/android/BbqcoinIntegration.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package de.schildbach.wallet.litecoin.integration.android;
+package de.schildbach.wallet.bbqcoin.integration.android;
 
 import android.app.Activity;
 import android.content.Context;
@@ -26,17 +26,17 @@ import android.widget.Toast;
 /**
  * @author Andreas Schildbach
  */
-public final class LitecoinIntegration
+public final class BbqcoinIntegration
 {
 	private static final String INTENT_EXTRA_TRANSACTION_HASH = "transaction_hash";
 
 	/**
-	 * Request any amount of Litecoins (probably a donation) from user, without feedback from the app.
+	 * Request any amount of Bbqcoins (probably a donation) from user, without feedback from the app.
 	 * 
 	 * @param context
 	 *            Android context
 	 * @param address
-	 *            Litecoin address
+	 *            Bbqcoin address
 	 */
 	public static void request(final Context context, final String address)
 	{
@@ -46,14 +46,14 @@ public final class LitecoinIntegration
 	}
 
 	/**
-	 * Request specific amount of Litecoins from user, without feedback from the app.
+	 * Request specific amount of Bbqcoins from user, without feedback from the app.
 	 * 
 	 * @param context
 	 *            Android context
 	 * @param address
-	 *            Litecoin address
+	 *            Bbqcoin address
 	 * @param amount
-	 *            Litecoin amount in nanocoins
+	 *            Bbqcoin amount in nanocoins
 	 */
 	public static void request(final Context context, final String address, final long amount)
 	{
@@ -63,18 +63,18 @@ public final class LitecoinIntegration
 	}
 
 	/**
-	 * Request any amount of Litecoins (probably a donation) from user, with feedback from the app. Result intent can be
+	 * Request any amount of Bbqcoins (probably a donation) from user, with feedback from the app. Result intent can be
 	 * received by overriding {@link android.app.Activity#onActivityResult()}. Result indicates either
 	 * {@link Activity#RESULT_OK} or {@link Activity#RESULT_CANCELED}. In the success case, use
 	 * {@link #transactionHashFromResult(Intent)} to read the transaction hash from the intent.
 	 * 
-	 * Warning: A success indication is no guarantee! To be on the safe side, you must drive your own Litecoin
+	 * Warning: A success indication is no guarantee! To be on the safe side, you must drive your own Bbqcoin
 	 * infrastructure and validate the transaction.
 	 * 
 	 * @param context
 	 *            Android context
 	 * @param address
-	 *            Litecoin address
+	 *            Bbqcoin address
 	 */
 	public static void requestForResult(final Activity activity, final int requestCode, final String address)
 	{
@@ -84,18 +84,18 @@ public final class LitecoinIntegration
 	}
 
 	/**
-	 * Request specific amount of Litecoins from user, with feedback from the app. Result intent can be received by
+	 * Request specific amount of Bbqcoins from user, with feedback from the app. Result intent can be received by
 	 * overriding {@link android.app.Activity#onActivityResult()}. Result indicates either {@link Activity#RESULT_OK} or
 	 * {@link Activity#RESULT_CANCELED}. In the success case, use {@link #transactionHashFromResult(Intent)} to read the
 	 * transaction hash from the intent.
 	 * 
-	 * Warning: A success indication is no guarantee! To be on the safe side, you must drive your own Litecoin
+	 * Warning: A success indication is no guarantee! To be on the safe side, you must drive your own Bbqcoin
 	 * infrastructure and validate the transaction.
 	 * 
 	 * @param context
 	 *            Android context
 	 * @param address
-	 *            Litecoin address
+	 *            Bbqcoin address
 	 */
 	public static void requestForResult(final Activity activity, final int requestCode, final String address, final long amount)
 	{
@@ -105,7 +105,7 @@ public final class LitecoinIntegration
 	}
 
 	/**
-	 * Put transaction hash into result intent. Meant for usage by Litecoin wallet applications.
+	 * Put transaction hash into result intent. Meant for usage by Bbqcoin wallet applications.
 	 * 
 	 * @param result
 	 *            result intent
@@ -119,11 +119,11 @@ public final class LitecoinIntegration
 	}
 
 	/**
-	 * Get transaction hash from result intent. Meant for usage by applications initiating a Litecoin payment.
+	 * Get transaction hash from result intent. Meant for usage by applications initiating a Bbqcoin payment.
 	 * 
-	 * You can use this hash to request the transaction from the Litecoin network, in order to validate. For this, you
-	 * need your own Litecoin infrastructure though. There is no guarantee that the transaction has ever been broadcasted
-	 * to the Litecoin network.
+	 * You can use this hash to request the transaction from the Bbqcoin network, in order to validate. For this, you
+	 * need your own Bbqcoin infrastructure though. There is no guarantee that the transaction has ever been broadcasted
+	 * to the Bbqcoin network.
 	 * 
 	 * @param result
 	 *            result intent
@@ -140,7 +140,7 @@ public final class LitecoinIntegration
 
 	private static Intent makeIntent(final String address, final Long amount)
 	{
-		final StringBuilder uri = new StringBuilder("litecoin:");
+		final StringBuilder uri = new StringBuilder("bbqcoin:");
 		if (address != null)
 			uri.append(address);
 		if (amount != null)
@@ -171,10 +171,10 @@ public final class LitecoinIntegration
 
 	private static void redirectToDownload(final Context context)
 	{
-		Toast.makeText(context, "No Litecoin application found.\nPlease install Litecoin Wallet.", Toast.LENGTH_LONG).show();
+		Toast.makeText(context, "No Bbqcoin application found.\nPlease install Bbqcoin Wallet.", Toast.LENGTH_LONG).show();
 
 		final Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=de.schildbach.wallet"));
-		final Intent binaryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://code.google.com/p/litecoin-wallet/downloads/list"));
+		final Intent binaryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://code.google.com/p/bbqcoin-wallet/downloads/list"));
 
 		final PackageManager pm = context.getPackageManager();
 		if (pm.resolveActivity(marketIntent, 0) != null)
